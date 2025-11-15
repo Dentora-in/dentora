c8f2552ebfc6b34ad69c00c4b989764a9c6a207d
Sanjana <sanjanashetty345@gmail.com>
2025-11-16 01:56:23 +0530

    deleted code

--- apps/server/src/controllers/appointment.controller.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const getAllAppointments = (req: Request, res: Response) => {
-    res.json({ message: "All appointments" });
-};
